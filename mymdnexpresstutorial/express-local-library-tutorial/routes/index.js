ce8da6ec58d98d5f25be0f25e40d450c4fd99724
Katie Baeten <katherinebaeten@gmail.com>
2022-05-07 09:43:56 -0500

    updated littleserver

--- mymdnexpresstutorial/express-local-library-tutorial/routes/index.js
+++ mymdnexpresstutorial/express-local-library-tutorial/routes/index.js
@@ -7,9 +7,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //res.render() is used to render a specified template along with the values of the named variables passed in an object, and then send the result as a response.
   res.render('index', { title: 'Express' });
 });
 //the "next" argument makes the callback a middleware function rather than a simple route callback. While the code doesn't currently use the next arguement, it may be useful in the future if you want to add multiple route handlers to the "/" route path
 
 //exports the router from the module
 module.exports = router;
+
+
+//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/skeleton_website
