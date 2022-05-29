3d715dd1f79ce7e72e368a2ee22e368a0b4b6578
Katie Baeten <katherinebaeten@gmail.com>
2022-05-29 12:01:32 -0500

    implementing graphiql

--- express_strapi_mongo/index.js
+++ express_strapi_mongo/index.js
@@ -3,17 +3,57 @@ var { graphqlHTTP } = require("express-graphql");
 var { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
+var contacts = [
+	{
+		name: "katie baeten",
+		age: 36,
+		email: "ktb8n.foo@gmail.com",
+		courses: [
+			{ number: "1.00", name: "MitXpro" },
+			{ number: "2.00", name: "Coping with MitXpro" },
+		],
+	},
+	{
+		name: "scooter beauregarde",
+		age: 11,
+		email: "noemail@cats.com",
+		courses: [
+			{ number: "1.00", name: "MitXpro" },
+			{ number: "3.00", name: "how to be less bitey" },
+		],
+	},
+	{
+		name: "eleanor stitch",
+		age: 3,
+		email: "whatsthat@cats.com",
+		courses: [
+			{ number: "1.00", name: "MitXpro" },
+			{ number: "3.00", name: "overcoming fear of people" },
+		],
+	},
+];
+
 var schema = buildSchema(`
   type Query {
-    hello: String
+    contact(id: Int) : Contact
+    contacts: [Contact]
+  },
+  type Contact {
+      name: String
+      email: String
+      age: Int
+      courses: [Course]
+  }
+  type Course {
+      number: String
+      name: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-	hello: () => {
-		return "Hello world!";
-	},
+    contact : (arg) => contacts[arg.id],
+    contacts : () => contacts
 };
 
 var app = express();
@@ -25,5 +65,8 @@ app.use(
 		graphiql: true,
 	})
 );
-app.listen(4000);
+app.listen(4000, () => {
+    console.log("this is the way the old dude likes to log it");
+});
+
 console.log("Running a GraphQL API server at http://localhost:4000/graphql");
